137e4d3df581a04191777ca1746bf1f2f92b1cf0
lumod97 <luiggigmd_97@outlook.com>
2023-07-17 01:40:39 -0500

    initial token successfully saved

--- app/Http/Controllers/ReceptDataController.php
+++ app/Http/Controllers/ReceptDataController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ReceptDataController extends Controller
 {
-    //
+    public function getInitialToken(Request $request)
+    {
+        $payload = [
+            'grant_type'=>'client_credentials',
+            'scope'=>'https://api.sunat.gob.pe/v1/contribuyente/contribuyentes',
+            'client_id'=>$request['secret_id'],
+            'client_secret'=>$request['secret_key']
+        ];
+        $response = Http::asForm()->post('https://api-seguridad.sunat.gob.pe/v1/clientesextranet/'.$request['secret_id'].'/oauth2/token', $payload);
+
+        $data = $response->json();
+        return $data;
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST,GET,PUT,PATCH,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    ['prefix' => 'settings'],
+    function () {
+        Route::post('get-initial-token', 'App\Http\Controllers\ReceptDataController@getInitialToken');
+    }
+);
